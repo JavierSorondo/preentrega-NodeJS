e1c49aed8b012436cb270b77b6815b4b96655f70
Javier Sorondo <jmsorondo@gmail.com>
2026-05-09 12:59:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+
+
+const args = process.argv.slice(2); 
+
+const method = args[0]?.toLowerCase();
+
+switch (method) {
+  case 'get':
+    get(args[1]);// uso el índice 1 para pasar solo los argumentos después del método
+    break;
+  case 'post':
+    post(args.slice(1));// uso slice para quitar "post" y pasar todos los demás argumentos a la función
+    break;
+    case 'delete':
+    deleteIt(args[1]);// es igual que get
+    break;
+  default:
+    console.error('Es necesario especificar un método válido: get, post o delete. Usaste: ' + method);
+    
+    process.exit(1);
+}
+
+
+function get(args) {
+  
+  if (args === "products") {// catálogo  completo
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  } else if (args.includes("products/")) {// producto específico
+    const productId = args.split("/")[1];
+    fetch(`https://fakestoreapi.com/products/${productId}`)
+      .then(response => response.json())
+      .then(data => console.log(data));
+  } else {
+    
+      console.error('Los argumentos para el método "get" son inválidos. Debe ser "products" o "products/{id}".');
+      process.exit(1);
+  }
+}
+
+
+function post(args) {
+  
+  const product = { 
+    title: args[1], 
+    price: args[2], 
+    description: args[3] 
+  };
+
+  if (!product.title || !product.price || !product.description) {
+    console.error('Para el método "post" se requieren los argumentos: title, price y description. Ejemplo: post "Nuevo Producto" 19.99 "Descripción del producto".');
+    process.exit(1);
+  }
+
+  fetch('https://fakestoreapi.com/products', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify(product)
+})
+  .then(response => response.json())
+  .then(data => console.log(`Producto agregado: ${data.title}`));
+}
+
+
+function deleteIt(args) {
+
+  if (args.includes("products/")) {
+    const productId = args.split("/")[1];
+    fetch(`https://fakestoreapi.com/products/${productId}`, {
+      method: 'DELETE'
+    })
+      .then(response => response.json())
+      .then(data => console.log(`Producto eliminado: ${data.title}`));
+  }
+}
